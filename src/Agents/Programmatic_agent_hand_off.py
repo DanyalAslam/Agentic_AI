e43283723354fe99d9e445ef412d45bf3b22ac5e
Danyal Aslam <danyal.aslam@sap.com>
2025-02-10 17:49:16 +0100

    fix: Flight and seat selection using dadigated agents each

--- src/Agents/Programmatic_agent_hand_off.py
+++ src/Agents/Programmatic_agent_hand_off.py
@@ -7,19 +7,16 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..')) # Targets to the src directory of the project
 
-from typing import Dict, List, Optional,Union
-import nest_asyncio
-from pydantic import BaseModel, Field
-from pydantic_ai import Agent, ModelRetry, RunContext, Tool
+from typing import Union
+import asyncio
+from pydantic_ai import Agent,RunContext, Tool
 from pydantic_ai.models.groq import GroqModel
 from pydantic_ai.models.openai import OpenAIModel
-import json
-import random
+from pydantic_ai.usage import Usage
 from rich.prompt import Prompt
 from pydantic_ai.messages import ModelMessage
-from pydantic_ai.usage import Usage, UsageLimits
-from Models.models import FlightDetails
-from Models.models import Failed
+from pydantic_ai.usage import Usage
+from Models.models import FlightDetails, Failed, SeatPreference
 from Api_Keys import api_key_openAI, api_key_groq
 
 
@@ -28,33 +25,83 @@ model_OpenAI_35_turbo = OpenAIModel('gpt-3.5-turbo-1106', api_key=api_key_openAI
 
 
 
-import asyncio
+
 
 #defining the first agent (Search flight agent)
 flight_search_agent = Agent[None, Union[FlightDetails, Failed]](
     model=model_Llama_33_70B,
      result_type=Union[FlightDetails, Failed],
        system_prompt=(
-        'Use the "flight_search" tool to find a flight '
+        'Use the "flight_search" tool to find a flight'
         'from the given origin to the given destination.'
     ),
     )
 
+@flight_search_agent.tool
+async def flight_search(ctx: RunContext[None], origing:str, destination:str) -> Union[FlightDetails, Failed]:
+    return FlightDetails(flight_number='AK456') # This line creates an instance of FlightDetails with the attribute flight_number set to 'AK456' and then returns that instance.
 
-async def find_flight() -> str:
+
+async def find_flight(usage: Usage) -> str:
+    message_history: Union[list[ModelMessage], None] = None
     for _ in range(3):
         prompt = Prompt.ask('where would you like to fly from and to?')
         result = await flight_search_agent.run(
             prompt,
-            usage_limits=UsageLimits(request_limit=5, total_tokens_limit=300),
+            message_history=message_history,
+            # usage_limits=UsageLimits(request_limit=5, total_tokens_limit=300),
         )
         if isinstance(result.data, FlightDetails):
             return result.data
+        else:
+             message_history = result.all_messages(
+                result_tool_return_content='Please try again.'
+            )
+
+
+# This agent is responsible for extracting the user's seat selection
+seat_preference_agent = Agent[None, Union[SeatPreference,None]](
+      model=model_Llama_33_70B,
+     result_type=Union[SeatPreference, Failed],
+       system_prompt=(
+      "Extract the user's seat preference."
+        'Seats A and F are window seats.'
+        'Row 1 is the front row and has extra leg room. '
+        'Rows 14, and 20 also have extra leg room. '
+    ),
+)
+
+
+
+async def find_seat(usage: Usage) -> SeatPreference:
+    message_history: Union[list[ModelMessage], None] = None
+    print("find_seat::ModelMessage", ModelMessage)
+    print("find_seat::message_history", message_history)
+    while True:
+        answer = Prompt.ask('What seat would you like?')
+
+        result = await seat_preference_agent.run(
+            answer,
+            message_history=message_history,
+        )
+        if isinstance(result.data, SeatPreference):
+            return result.data
+        else:
+            print('Could not understand seat preference. Please try again.')
+            message_history = result.all_messages()
+    
+
 
 
 async def main():
+    usage: Usage = Usage()
     print("Programmatic agent hand-off")
-    opt_flight_details = await find_flight()
+    opt_flight_details = await find_flight(usage)
+    print(" opt_flight_details", opt_flight_details)
+    if opt_flight_details is not None:
+        seat_preference = await find_seat(usage)
+        print("seat_preference", seat_preference)
+        
 
 
 
--- src/Models/models.py
+++ src/Models/models.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,  Field
+from typing import Literal, Union
+
+
+
 
 class FlightDetails(BaseModel):
     flight_number: str
 
-
 class Failed(BaseModel):
     """Unable to find a satisfactory choice."""
+
+
+class SeatPreference(BaseModel):
+    row: int = Field(ge=1, le=30)
+    seat: Literal['A', 'B', 'C', 'D', 'E', 'F']
+   
